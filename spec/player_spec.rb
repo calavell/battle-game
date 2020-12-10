1fe78d8fa1847d7f6ea3ad1522d0ff3fbabd1227
calavell <calavell@tcd.ie>
2020-12-10 15:41:10 +0000

    refactored tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ describe Player do
   describe '#print_name' do
     it 'prints the players own name' do
       player = Player.new('John')
-      expect(player.print_name).to eq 'John'
+      expect(player.name).to eq 'John'
     end
   end
 end
