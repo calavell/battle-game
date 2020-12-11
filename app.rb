b5eaefa4947fb8edfda7342ea32fa771e4ebfd76
ZeenLamDev <Zeen.Lam93@gmail.com>
2020-12-11 16:24:59 +0000

    skinny controllers

--- app.rb
+++ app.rb
@@ -11,22 +11,22 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-      # $player_1 = Player.new(params[:player_1_name])
-      # $player_2 = Player.new(params[:player_2_name])
+      player_1 = Player.new(params[:player_1_name])
+      player_2 = Player.new(params[:player_2_name])
       $game = Game.new(params[:player_1_name], params[:player_2_name])
       redirect '/play'
     end
 
   get '/play' do
-    @player_1 = $game.player_1
-    @player_2 = $game.player_2
+    # @player_1 = $game.player_1
+    # @player_2 = $game.player_2
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $game.player_1
-    @player_2 = $game.player_2
-    $game.attack
+    @game = $game
+    @game.attack
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,10 @@
 require 'game'
 
 describe Game do
-  let(:game) { described_class.new("Cathal", "Zeen") }
+  let(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+
   it 'is initializes with 2 players' do
     expect(Game).to respond_to(:new).with(2).arguments
   end
@@ -9,6 +12,12 @@ describe Game do
   describe '#attack' do
     it 'calls recieve_damage method on player' do
       expect{ game.attack }.to change { game.player_2.hp}.by(-10)
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2.name).to eq player_2
     end
   end
 end
